c2043284e425fb0ff62a2fc248e254d640015218
kimjonguk1 <kydmsqls@naver.com>
2024-12-02 16:18:34 +0900

    커밋

---
+++ src/main/java/dev/jwkim/jgv/JgvApplication.java
@@ -0,0 +1,13 @@
+package dev.jwkim.jgv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JgvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jwkim/jgv/JgvApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jwkim.jgv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
