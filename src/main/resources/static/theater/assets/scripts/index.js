4b75a939a318131cb49a666263d1e7ffd642f71e
escurse <av1967@naver.com>
2024-12-03 17:46:48 +0900

    [박재형] 영화관 관련 정보를 찾아오는 기능구현.

--- src/main/resources/static/theater/assets/scripts/index.js
+++ src/main/resources/static/theater/assets/scripts/index.js
@@ -1,11 +1,11 @@
 const $main = document.getElementById('main');
 const $items = $main.querySelector(':scope > .img > .main');
-const $move = Array.from($items.querySelectorAll(':scope > .item'));
+const $theater = Array.from($items.querySelectorAll(':scope > .item'));
 const $itemContainer = $main.querySelector(':scope > .img > .item-container');
 
-$move.forEach(($item) => {
+$theater.forEach(($item) => {
     $item.onclick = () => {
-        $move.forEach((x) => x.classList.remove('select'));
+        $theater.forEach((x) => x.classList.remove('select'));
         $item.classList.add('select');
         const url = new URL(location.href)
         url.searchParams.set('region', $item.innerText);
@@ -19,8 +19,10 @@ $move.forEach(($item) => {
                 return;
             }
             const response = JSON.parse(xhr.responseText);
+            let responses = [];
             $itemContainer.innerText = "";
             response['result'].forEach((x) => {
+                responses.push(x);
                 const $li = document.createElement('li');
                 $li.innerText = x['thName'];
                 $li.classList.add('item');
@@ -30,6 +32,58 @@ $move.forEach(($item) => {
                     $item.onclick = () => {
                         $containerItems.forEach((x) => x.classList.remove('select'));
                         $item.classList.add('select');
+                        responses.forEach((item) => {
+                            if (item['thName'] === $item.innerText) {
+                                //     $thName.innerText = item['thName'];
+                                //
+                                //     $thImage.innerHTML =
+                                //         `<img src="${item['thImg']}" alt=""
+                                //              class="image"/>;`
+                                //     console.log(item['thAddr'])
+
+                                const $theaterHead = new DOMParser().parseFromString(`
+        <div class="theater-container">        
+            <div class="header">
+                <span class="text">${item['thName']}</span>
+                <span class="stretch"></span>
+                <button class="button">단체/대관 문의</button>
+            </div>
+        </div>
+`, 'text/html').querySelector('.header');
+                                const $theater = new DOMParser().parseFromString(`
+    <div class="theater-container">
+        <div class="img">
+            <img src="${item['thImg']}" alt="" class="image">
+            <div class="theater-info-container">
+                <div class="small-container">
+                    <div class="header">
+                        <div class="info-container">
+                            <div class="theater-info">${item['thAddr']}</div>
+                        </div>
+                        <a href="#" class="theater-info guide" target="_blank">위치 / 주차 안내 ></a>
+                        <div class="stretch"></div>
+                        <div class="cinema-type">
+                            <a href="#" class="screenX" target="_blank"></a>
+                            <a href="#" class="screenX" target="_blank"></a>
+                            <a href="#" class="screenX" target="_blank"></a>
+                        </div>
+                    </div>
+                    <div class="theater-info">6관/874석</div>
+                </div>
+                <div class="stretch"></div>
+                <div class="notice-container">
+                    <div class="theater-info">공지사항</div>
+                    <a href="#" class="button"></a>
+                </div>
+            </div>
+        </div>
+    </div>`, 'text/html').querySelector('.img');
+                                const $theaterContainer = $main.querySelector(':scope > .theater-container')
+                                $theaterContainer.innerHTML = "";
+                                $theaterContainer.append($theaterHead);
+                                $theaterContainer.append($theater);
+                            }
+                        })
                     }
                 })
             })
@@ -38,3 +92,25 @@ $move.forEach(($item) => {
         xhr.send();
     }
 });
+
+const $buttonContainer = $main.querySelector(':scope > .button-container');
+const $buttons = Array.from($buttonContainer.querySelectorAll(':scope > .button'));
+const $informations = Array.from($main.querySelectorAll(':scope > .information'));
+const $selects = Array.from($buttonContainer.querySelectorAll(':scope > .button > .select'));
+$buttons.forEach(($item) => {
+    $item.onclick = () => {
+        $selects.forEach((x) => {
+            x.classList.add('hidden');
+            if (x.getAttribute('data-id') === $item.getAttribute('data-id')) {
+                x.classList.remove('hidden');
+            }
+        })
+        $informations.forEach((x) => {
+            x.classList.add('hidden');
+            if (x.getAttribute('data-id') === $item.getAttribute('data-id')) {
+                x.classList.remove('hidden');
+            }
+        });
+    }
+})
+
