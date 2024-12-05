72f05d774dbe6c99c643518c715d32eb75c3870d
kimjonguk1 <kydmsqls@naver.com>
2024-12-05 17:38:13 +0900

    [김종욱] 크롤링 기능 수정
    영화 제작국가 db insert 완료
    영화 제작국가 다대다 매핑 완료
    영화 인물 db insert 완료
    영화 인물 다대다 매핑 완료
    인물 이미지 db insert 완료

--- src/test/java/dev/jwkim/jgv/JgvApplicationTests.java
+++ src/test/java/dev/jwkim/jgv/JgvApplicationTests.java
@@ -1,13 +1,33 @@
 package dev.jwkim.jgv;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class JgvApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+        Document CharacterDoc = Jsoup.connect("http://www.cgv.co.kr/movies/detail-view/cast.aspx?midx=88847#menu").get();
+        Elements DirectorLinks = CharacterDoc.select("div.sect-staff-director > a");
+        Elements CharacterLinks = CharacterDoc.select("div.sect-staff-actor > a");
+        for (Element DirectorLink : DirectorLinks) {
+            try {
+                String DirectorHref = DirectorLink.attr("href");
+                DirectorHref = "http://www.cgv.co.kr" + DirectorHref;
+                Document DirectorDocs = Jsoup.connect(DirectorHref).get();
+                String DirectorName = DirectorDocs.select("div.box-contents > div.title > strong").text();
+                System.out.println(DirectorName);
+            } catch (Exception e) {
+                System.out.println("페이지가 존재하지 않습니다.");
+            }
+        }
     }
 
 }
