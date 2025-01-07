33f5048b794e2431eea7104baa1e1274beec23bb
kimjonguk1 <kydmsqls@naver.com>
2025-01-07 10:01:29 +0900

    [김종욱]수정

--- src/main/resources/static/assets/scripts/common.js
+++ src/main/resources/static/assets/scripts/common.js
@@ -41,33 +41,35 @@ class Loading {
 //검색 기능 추가
 document.addEventListener('DOMContentLoaded', () => {
     const $form = document.querySelector('.nav > .nav-wrapper > .search-wrapper > .search-form')
-    const $searchKeyword = $form.querySelector('.search > .keyword');
+    if($form) {
+        const $searchKeyword = $form.querySelector('.search > .keyword');
 
 
-    $form.onsubmit = (e) => {
-        e.preventDefault(); // 기본 폼 제출 동작 방지
+        $form.onsubmit = (e) => {
+            e.preventDefault(); // 기본 폼 제출 동작 방지
 
-        if ($searchKeyword.value === '') {
-            alert("검색어를 입력해 주세요");
-            return;
-        }
-
-        // GET 요청: 검색 키워드를 쿼리 파라미터로 전달
-        const searchUrl = `/movies/search?keyword=${encodeURIComponent($searchKeyword.value)}`;
-        const xhr = new XMLHttpRequest();
-
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState !== XMLHttpRequest.DONE) {
+            if ($searchKeyword.value === '') {
+                alert("검색어를 입력해 주세요");
                 return;
             }
-            if (xhr.status >= 200 && xhr.status < 300) {
-                window.location.href = searchUrl;
-            } else {
-                alert("서버가 알수 없는 응답을 반환하였습니다. 잠시 후 다시 시도해 주세요.");
-            }
-        };
 
-        xhr.open('GET', searchUrl);
-        xhr.send(); // GET 요청에는 본문(formData)이 필요하지 않습니다.
+            // GET 요청: 검색 키워드를 쿼리 파라미터로 전달
+            const searchUrl = `/movies/search?keyword=${encodeURIComponent($searchKeyword.value)}`;
+            const xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState !== XMLHttpRequest.DONE) {
+                    return;
+                }
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    window.location.href = searchUrl;
+                } else {
+                    alert("서버가 알수 없는 응답을 반환하였습니다. 잠시 후 다시 시도해 주세요.");
+                }
+            };
+
+            xhr.open('GET', searchUrl);
+            xhr.send(); // GET 요청에는 본문(formData)이 필요하지 않습니다.
+        }
     }
 })
